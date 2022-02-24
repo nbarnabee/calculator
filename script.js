08a110538d633a628346028f61d9723d81ab8bf9
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-02-24 13:57:54 +0100

    Add basic functionality

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 let calcArray = [];
 let calcString;
-const buttonSet = document.querySelectorAll(".numButton");
+let answer;
+const buttonSet = document.querySelectorAll(".calcButton");
+
+document.querySelector(".equals").onclick = calculateAnswer;
 
 Array.from(buttonSet).forEach(element => element.addEventListener('click', populateArray));
 
 function populateArray(click) {
-  calcArray.push(click.target.value);
-  console.log(calcArray);
+  if (click.target.value === "AC") {
+    calcArray.length = 0;
+  }
+
+  else if (click.target.value === "C") {
+    calcArray.pop();
+  }
+
+  else {
+    calcArray.push(click.target.value);
+  }
+
   calcString = calcArray.join("");
   document.getElementById("displayField").innerHTML = calcString;
+  return calcString;
+
+}
+
+
+function calculateAnswer() {
+  answer = eval(calcString);
+  document.getElementById("displayField").innerHTML = answer;
 }
